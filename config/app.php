1cc3880864a243b414c87f9e2d31cff680f85491
trukhina <trukhina@qarea.com>
2024-03-14 13:56:58 +0200

    Add jwt-auth

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\VoltServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ])->toArray(),
 
     /*
