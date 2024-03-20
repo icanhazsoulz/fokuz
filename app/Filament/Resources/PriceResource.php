723cbb876a1b86b5e36084254f75e50870e0f971
trukhina <trukhina@qarea.com>
2024-03-20 19:19:54 +0200

    Add price entity and filament resource

---
+++ app/Filament/Resources/PriceResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PriceResource\Pages;
+use App\Models\Price;
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PriceResource extends Resource
+{
+    protected static ?string $model = Price::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-euro';
+
+    protected static ?string $navigationGroup = 'Content';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->columnSpanFull(),
+                RichEditor::make('body')
+                    ->columnSpanFull(),
+                FileUpload::make('image')
+                    ->columnSpanFull(),
+                Checkbox::make('status')
+                    ->label('Published'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('title')
+                    ->wrap(),
+                Tables\Columns\TextColumn::make('body')
+                    ->html()
+                    ->words(50)
+                    ->wrap(),
+                Tables\Columns\CheckboxColumn::make('status')
+                    ->label('Published'),
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManagePrices::route('/'),
+        ];
+    }
+}
