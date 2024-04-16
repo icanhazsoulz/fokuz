c672844c6f28930a02787cb599e9ba1e601c5258
trukhina <trukhina@qarea.com>
2024-04-16 03:59:29 +0300

    Show call-me widget

--- resources/views/components/layouts/hero-wrapper.blade.php
+++ resources/views/components/layouts/hero-wrapper.blade.php
@@ -1,6 +1,7 @@
 <section class="h-dvh">
     <div class="container mx-auto relative">
         <livewire:socials-widget />
+        <x-widgets.call />
         {{ $slot }}
     </div>
 </section>
--- resources/views/components/widgets/call.blade.php
+++ resources/views/components/widgets/call.blade.php
@@ -1,12 +1,12 @@
 <a
     href="https://wa.me/4915773999273"
-    class="call flex w-56 gap-2 absolute z-50 top-[420px] right-5 no-underline text-font-color-1"
+    class="call flex gap-2 absolute z-50 top-[420px] right-5 no-underline text-font-color-1"
 >
     <div
-        class="flex flex-col justify-center text-xs bg-font-color-2 px-3 py-2 rounded shadow-2xl transition-all duration-200 hover:scale-105"
+        class="text-xs p-3 rounded shadow-2xl transition-all duration-200 hover:scale-105"
     >
-        Hast Du Fragen?<br>
-        <span class="font-bold">Schreib mir bei WhatsApp!</span>
+        <p>Hast Du Fragen?</p>
+        <p class="font-bold">Schreib mir bei WhatsApp!</p>
     </div>
     <div class="w-14 h-14">
         <img src="./assets/icons/call-me.svg" alt="WhatsApp Icon" class="w-full"/>
