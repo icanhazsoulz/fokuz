df0e2d3030fe3d590589b21329eff84d4b24cd3a
trukhina <trukhina@qarea.com>
2024-05-07 01:07:22 +0300

    Add message test

--- tests/Feature/Livewire/CreateMessage/CreateMessageTest.php
+++ tests/Feature/Livewire/CreateMessage/CreateMessageTest.php
@@ -4,10 +4,8 @@ namespace Tests\Feature\Livewire\CreateMessage;
 
 use App\Livewire\Forms\CreateMessage;
 use App\Models\Message;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Hash;
 use Livewire\Livewire;
 use Spatie\Permission\Models\Role;
 use Tests\TestCase;
@@ -16,20 +14,37 @@ class CreateMessageTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_can_create_message()
+    public function test_new_client_can_create_message()
     {
         Role::create(['name' => 'client']);
 
-        $this->assertEquals(0, Message::count());
+        $this->assertEquals(0, Message::all()->count());
+        $this->assertEquals(0, User::all()->count());
 
         Livewire::test(CreateMessage::class)
             ->set('form.email', 'arya.stark@winterfell.org')
-            ->set('form.phone', fake()->phoneNumber)
-            ->set('form.name', 'Arya')
-            ->set('form.lastName', 'Stark')
+            ->set('form.name', 'Arya Stark')
             ->set('form.message', fake()->text(200))
             ->call('save');
 
-        $this->assertEquals(1, Message::count());
+        $this->assertEquals(1, Message::all()->count());
+        $this->assertEquals(1, User::all()->count());
     }
+
+    public function test_existing_client_can_create_message()
+    {
+        $client = $this->create_client();
+        $this->assertEquals(0, $client->messages->count());
+
+        Livewire::test(CreateMessage::class)
+            ->set('form.email', $client->email)
+            ->set('form.name', $client->name)
+            ->set('form.message', fake()->text(200))
+            ->call('save')
+        ;
+        $client->refresh();
+        $this->assertEquals(1, $client->messages->count());
+    }
+
+    // TODO: logged in client can see their data in the form
 }
