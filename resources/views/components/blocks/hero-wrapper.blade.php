c3fb66a709ed6e8cb8b0a9504a73d971b9e6bc75
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2024-05-10 12:27:56 +0200

    start: home page first screen

--- resources/views/components/blocks/hero-wrapper.blade.php
+++ resources/views/components/blocks/hero-wrapper.blade.php
@@ -15,6 +15,14 @@
     </div>
     @if(request()->is('/'))
         <div class="container mx-auto">
+            <div class="pt-10 flex justify-center items-center">
+                <div class="rounded relative before:content-[''] before:absolute before:block before:w-1/3 before:aspect-square before:-top-10 before:-right-16 before:z-0 before:bg-[#EDDEBA] before:rounded after:content-[''] after:absolute after:block after:w-2/3 after:h-1/2 after:aspect-square after:-bottom-10 after:-left-10 after:bg-font-color-2 after:z-0 after:rounded max-w-[713px]">
+                    <div class="relative z-20">
+                        <img src="./assets/images/home-page/hero-screen.jpg" alt="black cat">
+                    </div>
+                </div>
+            </div>
+
             <h1 class="text-4xl">
                 Mit jeder Photobestellung spenden Sie <br />
                 <span
