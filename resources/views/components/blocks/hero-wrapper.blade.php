dbb9046c4d4479b130079f77c0f93ac7eb03a1ff
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2024-05-23 01:22:35 +0200

    feat: update tailwind config background routes

--- resources/views/components/blocks/hero-wrapper.blade.php
+++ resources/views/components/blocks/hero-wrapper.blade.php
@@ -14,8 +14,8 @@
             <x-button-primary class="absolute bottom-44 left-12  z-30">
                 Frag mich mal
             </x-button-primary>
-            <div class="pt-10 flex justify-center items-center bg-hero relative">
-                <img src="../assets/images/home-page/start-screen-bg.png" alt="white points" class="absolute left-0 top-0">
+            <div class="pt-10 flex justify-center items-center bg-hero bg-no-repeat relative">
+                {{--<img src="../assets/images/home-page/start-screen-bg.png" alt="white points" class="absolute left-0 top-0">--}}
                 <div class="rounded relative before:content-[''] before:absolute before:block before:w-1/3 before:aspect-square before:-top-10 before:-right-16 before:z-0 before:bg-[#EDDEBA] before:rounded after:content-[''] after:absolute after:block after:w-2/3 after:h-1/2 after:aspect-square after:-bottom-10 after:-left-10 after:bg-white after:z-10 after:rounded max-w-[713px]">
                     <div class="relative z-20">
                         <img src="./assets/images/home-page/hero-screen.jpg" alt="black cat">
