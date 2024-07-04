dc33107d7fb4911fc25748f0ef1b32545a98a6d3
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2024-07-05 00:44:36 +0200

    correct discount block rabatt from rabbat

--- resources/views/components/blocks/cta-cols.blade.php
+++ resources/views/components/blocks/cta-cols.blade.php
@@ -4,7 +4,7 @@
 
 
 <div {{ $attributes->merge(['class' => $classes]) }}>
-    <x-discount-header>100% Rabbat</x-discount-header>
+    <x-discount-header>100% Rabatt</x-discount-header>
     <div class="w-1/2 flex flex-col justify-between items-start grow py-5 px-[5%]">
         <x-header-medium>Ein beliebiges Fotoshooting für Dein aus dem Tierheim adoptiertes Tier</x-header-medium>
         <x-button-primary>Frag mich mal</x-button-primary>
--- resources/views/components/blocks/cta-rows.blade.php
+++ resources/views/components/blocks/cta-rows.blade.php
@@ -4,7 +4,7 @@
 
 
 <div {{ $attributes->merge(['class' => $classes]) }}>
-    <x-discount-header>50% Rabbat</x-discount-header>
+    <x-discount-header>50% Rabatt</x-discount-header>
     <div class="flex justify-between gap-[10%] px-[10%] py-6">
         <x-header-medium class="max-w-[550px] grow">beim Fotoshootings am Geburtstag Deines Tieres</x-header-medium>
         <x-button-primary>Frag mich mal</x-button-primary>
